20f00822ced05b626b1dc957a33f358eed169dad
Michael Dyrynda <michael@dyrynda.com.au>
2025-10-15 13:01:57 +1030

    add tests for default version behaviour

--- tests/Feature/GeneratesUuidTest.php
+++ tests/Feature/GeneratesUuidTest.php
@@ -30,4 +30,61 @@ class GeneratesUuidTest extends TestCase
             'The UUID column should match the configured value.'
         );
     }
+
+    #[Test]
+    public function it_inherits_uuid_version_from_config()
+    {
+        Config::set('model-uuid.uuid_version', 'uuid1');
+
+        $testClass = new class
+        {
+            use GeneratesUuid;
+        };
+
+        $this->assertSame('uuid1', $testClass->resolveUuidVersion());
+    }
+
+    #[Test]
+    public function it_defaults_to_uuid4_when_config_not_set()
+    {
+        Config::set('model-uuid.uuid_version', null);
+
+        $testClass = new class
+        {
+            use GeneratesUuid;
+        };
+
+        $this->assertSame('uuid4', $testClass->resolveUuidVersion());
+    }
+
+    #[Test]
+    public function it_defaults_to_uuid4_when_config_is_empty()
+    {
+        Config::set('model-uuid.uuid_version', '');
+
+        $testClass = new class
+        {
+            use GeneratesUuid;
+        };
+
+        $this->assertSame('uuid4', $testClass->resolveUuidVersion());
+    }
+
+    #[Test]
+    public function it_uses_model_definition_as_highest_precedence()
+    {
+        Config::set('model-uuid.uuid_version', 'uuid7');
+
+        $testClass = new class
+        {
+            use GeneratesUuid;
+
+            public function uuidVersion(): ?string
+            {
+                return 'uuid6';
+            }
+        };
+
+        $this->assertSame('uuid6', $testClass->resolveUuidVersion());
+    }
 }
